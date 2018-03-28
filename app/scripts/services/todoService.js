b721a3c53f99891020acf1645b6893243d16684f
aravind swarna <aravindswarana@gmail.com>
2018-03-28 15:30:49 +0530

    Moving out the address

--- app/scripts/services/todoService.js
+++ app/scripts/services/todoService.js
@@ -9,23 +9,23 @@
  */
  angular.module('todoAppApp')
   .factory('TodoFactory', ['$http',function($http) {
-
+  		var path='http://localhost:8080';
   	return {
   		getTodos:function()
   		{
-  			return $http.get('http://localhost:8080/api/todo');
+  			return $http.get(path+'/api/todo');
   		},
   		createTodo:function(data)
   		{
-  			return $http.post('http://localhost:8080/api/todo',data);
+  			return $http.post(path+'/api/todo',data);
   		},
   		deleteTodo:function(id)
   		{
-  			return $http.delete('http://localhost:8080/api/todo/'+id);
+  			return $http.delete(path+'/api/todo/'+id);
   		},
   		updateTodo:function(data)
   		{
-  			return $http.put('http://localhost:8080/api/todo',data);
+  			return $http.put(path+'/api/todo',data);
   		}
   	}
 
